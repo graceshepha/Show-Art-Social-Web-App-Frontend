a947511c5c6a7e5636d259cb629e00923c3d71de
Roger Montero <rogesteban0@gmail.com>
2022-02-09 02:00:05 -0500

    auth callback

--- pages/api/auth/[...auth0].ts
+++ pages/api/auth/[...auth0].ts
@@ -1,3 +1,38 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
+import { AfterCallback, handleAuth, handleCallback } from "@auth0/nextjs-auth0";
 
-export default handleAuth();
+type UserDetails = {
+  username: String;
+  email: String;
+  picture: String;
+  email_verified: boolean;
+};
+
+/*
+  User: {
+    nickname,
+    name (email),
+    picture,
+    updated_at,
+    email,
+    email_verified,
+    sub
+  }
+ */
+
+const afterCallback: AfterCallback = (req, res, session, state) => {
+  console.log("User: ");
+  console.log(session.user) // get user details
+  // TODO send to backend
+  return session;
+};
+
+export default handleAuth({
+  callback: async (req, res) => {
+    // try {
+    //   await handleCallback(req, res, { afterCallback });
+    // } catch (error) {
+    //   res.status(error.status || 500).end(error.message);
+    // }
+    await handleCallback(req, res, { afterCallback });
+  },
+});
